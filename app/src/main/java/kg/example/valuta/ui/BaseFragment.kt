2488ecce9d7e1e2511f339a9baed0c5ad351c9cf
Emil <emil.mametov.01@gmail.com>
2022-11-18 13:53:23 +0600

    Initial commit

---
+++ app/src/main/java/kg/example/valuta/ui/BaseFragment.kt
@@ -0,0 +1,33 @@
+package kg.example.less2.Base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+
+
+abstract class BaseFragment<VB : ViewBinding, VM: ViewModel>: Fragment() {
+    protected lateinit var binding: VB
+    protected lateinit var viewModel: VM
+     abstract fun inflateVB(layoutInflater: LayoutInflater): VB
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = inflateVB(layoutInflater)
+        return binding.root
+        initView()
+        initViewModel()
+        initListener()
+    }
+
+    abstract fun initListener()
+    abstract fun initViewModel()
+
+    abstract fun initView()
+}
---
+++ app/src/main/java/kg/example/valuta/ui/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package kg.example.youtubeapi.core.ui
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel: ViewModel() {
+}
